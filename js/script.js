6ee5d755995b210c5c569ad851dba72b8d0a7d30
Novyk Aliona <alionanovyk99@gmail.com>
2021-02-10 00:00:42 +0200

    Added DragNDrop

--- js/script.js
+++ js/script.js
@@ -2,7 +2,12 @@
 
 window.addEventListener('DOMContentLoaded', () => {
     const members = ["Maria", "Sergey", "Aliona", "Oleg", "Boryslav"];
+    const eventCell = document.querySelectorAll('.event_cell');
+    eventCell.forEach(item => {
+        item.setAttribute('data-dropcont', 'true');
+    });
 
+    
     function removeAllEvents(selector){
         const container = document.querySelectorAll(selector);
         container.forEach((item) => {
@@ -47,6 +52,16 @@ window.addEventListener('DOMContentLoaded', () => {
         })
         .then(response => response.json());
     }
+    async function putObject(url, data){
+        return await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: data
+        })
+        .then(response => response.json());
+    }
     function createEventVisual(eventName, members, day, time, id, inputSelector) {
 
         const input = document.querySelector(inputSelector);      
@@ -58,14 +73,28 @@ window.addEventListener('DOMContentLoaded', () => {
         event.setAttribute('data-day', day);
         event.setAttribute('data-time', time);
         event.setAttribute('data-id', id);
+        event.setAttribute('draggable', 'true');
 
         const crossBtn = document.createElement('span');
         crossBtn.classList.add('cross');
         event.append(crossBtn);
         input.append(event);
-          
     }
     
+    function createErrorElement(errorTitleText, container){
+        const error = document.createElement('div');
+        error.classList.add('error');
+        const errorIcon = document.createElement('div');
+        errorIcon.classList.add('error__icon');
+        errorIcon.textContent = '×';
+        const errorTitle = document.createElement('div');
+        errorTitle.classList.add('error__title');
+        errorTitle.textContent = errorTitleText;
+        error.append(errorIcon);
+        error.append(errorTitle);
+        document.querySelector(container).prepend(error);
+    }
+
     if(document.querySelector('.calendar')){
 
         createOptions(members, '.header__filter');
@@ -85,11 +114,49 @@ window.addEventListener('DOMContentLoaded', () => {
                         
                         deleteObject(`http://localhost:3000/events`, dataToDelete)
                         .then(data => {
-                            console.log(data);
-            
                             window.location.reload();
                         });
                     }); 
+                });
+
+
+                const draggableElements = document.querySelectorAll('[draggable="true"]'),
+                      dragContainer = document.querySelector('.calendar');
+
+                draggableElements.forEach(element => {
+                    element.addEventListener('dragstart', (e) => {
+                        e.target.classList.add('selected');
+                    });
+                    dragContainer.addEventListener('dragover', (e) => {
+                        e.preventDefault();
+
+                        const activeElement = document.querySelector('.selected'),
+                            currentElement = e.target;
+
+                        let isMoveable = activeElement !== currentElement && currentElement.getAttribute('data-dropcont') == "true" && currentElement.childNodes.length === 0;
+                        
+                        if(!isMoveable){
+                            return;
+                        }
+                        activeElement.setAttribute('data-day', currentElement.getAttribute('data-slot').substring(0, 3));
+                        activeElement.setAttribute('data-time', currentElement.getAttribute('data-slot').slice(-2));
+                        currentElement.append(activeElement);
+
+                        const idToPut = activeElement.getAttribute('data-id');
+                        
+                        putObject(`http://localhost:3000/events/${idToPut}`, JSON.stringify({
+                            id: idToPut,
+                            name: activeElement.textContent,
+                            participants: activeElement.getAttribute('data-members'),
+                            day: activeElement.getAttribute('data-day'),
+                            time: activeElement.getAttribute('data-time')
+                        }));
+                        return false;
+                    });
+                    element.addEventListener('dragend', (e) => {
+                        e.target.classList.remove('selected');
+                    });
+                    
                 });
                 
             });
@@ -112,9 +179,7 @@ window.addEventListener('DOMContentLoaded', () => {
                         const dataToDelete = thisEventID.getAttribute('data-id');
                         
                         deleteObject(`http://localhost:3000/events`, dataToDelete)
-                        .then(data => {
-                            console.log(data);
-            
+                        .then(data => {            
                             window.location.reload();
                         });
                     }); 
@@ -129,6 +194,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
             window.location = 'new-event.html';
         });
+
+    
     }
 
     if(document.querySelector('.newevent')){
@@ -154,61 +221,37 @@ window.addEventListener('DOMContentLoaded', () => {
             form.addEventListener('submit', (e) => {
                 e.preventDefault();
 
-                const formData = new FormData(form);
-                const participantsOptions = document.querySelectorAll('.newevent__memb-select option:checked');
-                const participantsArray = [];
-                participantsOptions.forEach(item => {
-                    participantsArray.push(item.value);
-                });
-                console.log(participantsArray);
-                formData.append('participants', participantsArray);
-                const formDataToObject = Object.fromEntries(formData.entries());
-                const formDataDayTime = `${formDataToObject.day}${formDataToObject.time}`;
-                if (dayTimeArray.indexOf(formDataDayTime) != -1){
-                    document.querySelector('.error').classList.add('show-flex');
-                    document.querySelector('.error').classList.remove('hide');
+                const inputName = form.querySelector('.newevent__name-input'),
+                      selectParticipans = form.querySelectorAll('.newevent__memb-select option:checked');
+                if(inputName.value.length == 0){
+                    createErrorElement('Please, input event name', '[for="event-name"]');
+                } else if(selectParticipans.length < 2){
+                    createErrorElement('Please, choose 2 or more participants', '[for="participants"]');
                 } else{
-                    const json = JSON.stringify(Object.fromEntries(formData.entries()));
-                    console.log(json.name);
-                    postData('http://localhost:3000/events', json, 'POST')
-                    .then(data => {
-                        console.log(data);
-    
-                        window.location = 'index.html';
+                    const formData = new FormData(form);
+                    const participantsOptions = document.querySelectorAll('.newevent__memb-select option:checked');
+                    const participantsArray = [];
+                    participantsOptions.forEach(item => {
+                        participantsArray.push(item.value);
                     });
+                    
+                    formData.append('participants', participantsArray);
+                    const formDataToObject = Object.fromEntries(formData.entries());
+                    const formDataDayTime = `${formDataToObject.day}${formDataToObject.time}`;
+                    if (dayTimeArray.indexOf(formDataDayTime) != -1){
+                        createErrorElement('Failed to create an event. Time slot is already booked', 'form');
+                        document.querySelector('form > .error').classList.add('show-flex');
+                        document.querySelector('form > .error').classList.remove('hide');
+                    } else{
+                        const json = JSON.stringify(Object.fromEntries(formData.entries()));
+                    
+                        postData('http://localhost:3000/events', json, 'POST')
+                        .then(data => {   
+                            window.location = 'index.html';
+                        });
+                    }
                 }
-                
             });
         }
     }
-
-    // const deleteBtns = document.querySelectorAll('.calendar .cross');
-
-    // const postData = async (url, data, method) => {
-    //     const result = await fetch(url, {
-    //         method: method,
-    //         headers: {
-    //             'Content-type': 'application/json',
-    //         },
-    //         body: data,
-    //     });
-    //     return await result.json();
-    // };
-
-    // function deleteData(btns){
-    //     btns.forEach(btn => {
-    //         btn.addEventListener('click', (e) => {
-    //             e.preventDefault();
-
-    //             postData('http://localhost:3000/events', btn.closest('.event'), 'DELETE')
-    //              .then(data => {
-    //                  console.log(data);
-    //              })
-
-    //         });
-    //     });
-    // }
-    // deleteBtns.forEach(btn => {
-    //     deleteData(btn)
-    // });
 });
